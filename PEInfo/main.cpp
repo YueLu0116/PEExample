da0d5ac841c8f5e46143af405e715c4235ea653d
YueLu0116 <bit_drinkwater@163.com>
2024-01-07 15:53:30 +0800

    feat: iterate resource table

--- PEInfo/main.cpp
+++ PEInfo/main.cpp
@@ -14,8 +14,10 @@ static PEFileInfo g_peFileInfo{};
 
 bool OpenPEFile(const std::string& strPEPath, LPVOID* pStartAddress);
 bool CheckPEFile(PIMAGE_DOS_HEADER pImageDOSHeader);
-bool GetPEImportInfo();
 DWORD RVA2FOV(PIMAGE_NT_HEADERS pImageNTHeaders, DWORD dwRVA);
+
+bool GetPEImportInfo();
+bool GetPEResourceInfo();
 
 int main(int argc, char** argv)
 {
@@ -40,7 +42,8 @@ int main(int argc, char** argv)
 		return -3;
 	}
 
-	GetPEImportInfo();
+	//GetPEImportInfo();
+	GetPEResourceInfo();
 
 	return 0;
 }
@@ -186,4 +189,113 @@ DWORD RVA2FOV(PIMAGE_NT_HEADERS pImageNTHeaders, DWORD dwRVA)
 	}
 
 	return dwRet;
+}
+
+bool GetPEResourceInfo()
+{
+	if (!g_peFileInfo.pImageNTHeaders || !g_peFileInfo.pStartAddress)
+	{
+		return false;
+	}
+
+	auto pPEFileHeader = g_peFileInfo.pImageNTHeaders;
+
+	if (!pPEFileHeader->OptionalHeader.DataDirectory[2].VirtualAddress)  // resource table
+	{
+		return false;
+	}
+
+	DWORD dwOffsetResourceTable = RVA2FOV(pPEFileHeader, pPEFileHeader->OptionalHeader.DataDirectory[2].VirtualAddress);
+	DWORD dwResourceTableAddr = (unsigned long)(g_peFileInfo.pStartAddress) + dwOffsetResourceTable;
+
+	IMAGE_RESOURCE_DIRECTORY* pImageResourceDir = (IMAGE_RESOURCE_DIRECTORY*)dwResourceTableAddr;
+	const WORD usNumberOfNamedEntries = pImageResourceDir->NumberOfNamedEntries;
+	const WORD usNumberOfIdEntries = pImageResourceDir->NumberOfIdEntries;
+	const WORD usTotalNumberOfEntries = usNumberOfNamedEntries + usNumberOfIdEntries;
+
+	IMAGE_RESOURCE_DIRECTORY_ENTRY* pImageResDirEntry = (IMAGE_RESOURCE_DIRECTORY_ENTRY*)((DWORD)pImageResourceDir + sizeof(IMAGE_RESOURCE_DIRECTORY));
+
+	for (size_t idEntryFirstLevel = 0; idEntryFirstLevel < usTotalNumberOfEntries; idEntryFirstLevel++)
+	{
+		// level 1
+		if (!pImageResDirEntry[idEntryFirstLevel].NameIsString)
+		{
+			if (pImageResDirEntry[idEntryFirstLevel].Id <= 0x18)  // ref: https://learn.microsoft.com/en-us/windows/win32/menurc/resource-types
+			{
+				printf("System resource id: %d\n", pImageResDirEntry[idEntryFirstLevel].Id);
+			}
+			else
+			{
+				char szType[32];
+				sprintf_s(szType, "%d", pImageResDirEntry[idEntryFirstLevel].Id);
+				printf("Customed resource id:%d %s\n", pImageResDirEntry[idEntryFirstLevel].Id, szType);
+			}
+		}
+		else 
+		{
+			PIMAGE_RESOURCE_DIR_STRING_U pImageResDirStr = (PIMAGE_RESOURCE_DIR_STRING_U)((DWORD)pImageResourceDir + pImageResDirEntry[idEntryFirstLevel].NameOffset);
+			WCHAR szStr[MAX_PATH];
+			memcpy_s(szStr, MAX_PATH, pImageResDirStr->NameString, pImageResDirStr->Length * sizeof(WCHAR));
+			szStr[pImageResDirStr->Length] = L'\0';
+			printf("Resource name£º%ls \n", szStr);
+		}
+
+		// level 2
+		if (pImageResDirEntry[idEntryFirstLevel].DataIsDirectory)
+		{
+			printf("\t## Second level dir\n");
+			IMAGE_RESOURCE_DIRECTORY* pImageResDirLevel2 = (IMAGE_RESOURCE_DIRECTORY*)((DWORD)pImageResourceDir + pImageResDirEntry[idEntryFirstLevel].OffsetToDirectory);
+			IMAGE_RESOURCE_DIRECTORY_ENTRY* pImageResDirEntryLevel2 = (IMAGE_RESOURCE_DIRECTORY_ENTRY*)(pImageResDirLevel2 + 1);
+			const WORD usTotalNumOfEntriesLevel2 = pImageResDirLevel2->NumberOfIdEntries + pImageResDirLevel2->NumberOfNamedEntries;
+
+			for (size_t idEntrySecondLevel = 0; idEntrySecondLevel < usTotalNumOfEntriesLevel2; idEntrySecondLevel++)
+			{
+				if (!pImageResDirEntryLevel2[idEntrySecondLevel].NameIsString)
+				{
+					printf("\tResource id: %d\n", pImageResDirEntryLevel2[idEntrySecondLevel].Id);
+				}
+				else
+				{
+					PIMAGE_RESOURCE_DIR_STRING_U pImageResDirStr = (PIMAGE_RESOURCE_DIR_STRING_U)((DWORD)pImageResourceDir + pImageResDirEntryLevel2[idEntrySecondLevel].NameOffset);
+					WCHAR szStr[MAX_PATH];
+					memcpy_s(szStr, MAX_PATH, pImageResDirStr->NameString, pImageResDirStr->Length * sizeof(WCHAR));
+					printf("\tResource name£º%ls \n", szStr);
+				}
+
+				// level 3
+				if (pImageResDirEntryLevel2[idEntrySecondLevel].DataIsDirectory)
+				{
+					printf("\t\t### Third level dir\n");
+					IMAGE_RESOURCE_DIRECTORY* pImageResDirLevel3 = (IMAGE_RESOURCE_DIRECTORY*)((DWORD)pImageResourceDir + pImageResDirEntryLevel2[idEntrySecondLevel].OffsetToDirectory);
+					IMAGE_RESOURCE_DIRECTORY_ENTRY* pImageResDirEntryLevel3 = (IMAGE_RESOURCE_DIRECTORY_ENTRY*)(pImageResDirLevel3 + 1);
+					const WORD usTotalNumOfEntriesLevel3 = pImageResDirLevel3->NumberOfIdEntries + pImageResDirLevel3->NumberOfNamedEntries;
+
+					for (size_t idEntryThirdLevel = 0; idEntryThirdLevel < usTotalNumOfEntriesLevel3; idEntryThirdLevel++)
+					{
+						if (!pImageResDirEntryLevel3[idEntryThirdLevel].NameIsString)
+						{
+							printf("\t\tResource id: %d\n", pImageResDirEntryLevel3[idEntryThirdLevel].Id);
+						}
+						else
+						{
+							PIMAGE_RESOURCE_DIR_STRING_U pImageResDirStr = (PIMAGE_RESOURCE_DIR_STRING_U)((DWORD)pImageResourceDir + pImageResDirEntryLevel3[idEntryThirdLevel].NameOffset);
+							WCHAR szStr[MAX_PATH];
+							memcpy_s(szStr, MAX_PATH, pImageResDirStr->NameString, pImageResDirStr->Length * sizeof(WCHAR));
+							printf("\t\tResource name£º%ls \n", szStr);
+						}
+
+						if (!pImageResDirEntryLevel3[idEntryThirdLevel].DataIsDirectory)
+						{
+							IMAGE_RESOURCE_DATA_ENTRY* pImageResourceDataEntry = (IMAGE_RESOURCE_DATA_ENTRY*)((DWORD)pImageResourceDir + pImageResDirEntryLevel3[idEntryThirdLevel].OffsetToData);
+							printf("\t\tResource data offset=0x%x, data size=%d\n", pImageResourceDataEntry->OffsetToData, pImageResourceDataEntry->Size);
+						}
+					}
+				}
+
+			}
+		}
+
+	}
+
+	return true;
 }
